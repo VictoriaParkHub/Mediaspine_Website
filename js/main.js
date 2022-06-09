7566ad9612f858e5b840b25f4ff3fa8e97738328
Victoria Park <hyerim7312@gmail.com>
2022-06-09 17:01:15 +0900

    subpage : system_CRRS

--- js/main.js
+++ js/main.js
@@ -1,66 +1,5 @@
 "use strict";
 
-//scrolling 변수
-const nav = document.querySelector("#nav");
-const headerHeight = nav.getBoundingClientRect().height;
-const mainmenu = document.querySelectorAll("#mainmenu");
-//문의하기 버튼 변수
-const contactbtn = document.querySelector(".contactbtn");
-
-//submenubg 변수
-const submenubg = document.querySelector('.nav__submenu__bg');
-const submenucontainer = document.querySelectorAll('.nav__submenu__container');
-
-
-//scrolling -> headerHeight값으로 if문
-window.addEventListener("scroll", () => {
-  if (window.scrollY > headerHeight) {
-    nav.setAttribute("style", "background-color:white");
-    document.getElementById("logo").src="css/img/logo2.png";
-    mainmenu.forEach(mainmenu => mainmenu.setAttribute("style", "color:black"));
-    contactbtn.classList.remove("contactbtn");
-    contactbtn.classList.add("contactbtn2");
-  }
-  else {
-    nav.setAttribute("style", "background: transparent;");
-    document.getElementById("logo").src="css/img/logo.png";
-    mainmenu.forEach(mainmenu => mainmenu.setAttribute("style", "color:white"));
-    contactbtn.classList.remove("contactbtn2");
-    contactbtn.classList.add("contactbtn");
-  }
-});
-
- //mouseover하면 submenubg가 보임
-function mouseOver(){
-  submenubg.setAttribute("style", "visibility: visible");
-  document.getElementById("logo").src="css/img/logo2.png";
-  //queryselectorAll은 반복문인 forEach와 함께 사용해준다. (계속 한개만 선택되니까)
-  //mainmenu.forEach(mainmenu => mainmenu.setAttribute("style", "color:black")); -> 화살표 함수
-  mainmenu.forEach(function(mainmenu) {
-    mainmenu.setAttribute("style", "color:black");
-  }
-  );
-  submenucontainer.forEach(function(submenucontainer) {
-    submenucontainer.setAttribute("style", "display:block");
-  }
-  );
-}
-function mouseOut(){
-  submenubg.setAttribute("style", "visibility: hidden");
-  document.getElementById("logo").src="css/img/logo.png";
-  mainmenu.forEach(mainmenu => mainmenu.setAttribute("style", "color:white"));
-  submenucontainer.forEach(function(submenucontainer) {
-    submenucontainer.setAttribute("style", "display:none");
-  }
-  );
-
-  if(window.scrollY > headerHeight){
-    mainmenu.forEach(mainmenu => mainmenu.setAttribute("style", "color:black"));
-    document.getElementById("logo").src="css/img/logo2.png";
-  }
-};
-
-
   //mouseover하면 gallery img가 펼쳐짐
 
   const galleryImg01 = document.querySelector('.galleryImg01');
@@ -134,8 +73,7 @@ function mouseOut(){
     gallery04.classList.remove("containerover");
   };
 
-
-//Scroll Fade-in, out
+  //Scroll Fade-in, out
 
 const targets = document.querySelectorAll(".fade-class");
 const options = { root: null, threshold: 0.1, rootMargin: "-0px" };
@@ -155,57 +93,52 @@ targets.forEach((target) => {
 });
 
 
-
 window.onload = function () {
 
+    //scroll snap event - Crome
 
-//scroll snap event - Crome
-
-const elm = document.querySelectorAll('.scroll');
-const elmCount = elm.length;
-
-//chrome
-elm.forEach(function(item, index){
-  item.addEventListener('mousewheel', function(event){
-    event.preventDefault();
-    let delta = 0;
-
-    if (!event) event = window.event;
-    if (event.wheelDelta) {
-        delta = event.wheelDelta / 120;
-        if (window.opera) delta = -delta;
-    } 
-    else if (event.detail)
-        delta = -event.detail / 3;
-
-    let moveTop = window.scrollY;
-    let elmSelector = elm[index];
-
-    // wheel down : move to next section
-    if (delta < 0){
-      if (elmSelector !== elmCount-1){
-        try{
-          moveTop = window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
-        }catch(e){}
-      }
-    }
-    // wheel up : move to previous section
-    else{
-      if (elmSelector !== 0){
-        try{
-          moveTop = window.pageYOffset + elmSelector.previousElementSibling.getBoundingClientRect().top;
-        }catch(e){}
-      }
-    }
-
-    const body = document.querySelector('html');
-    window.scrollTo({top:moveTop, left:0, behavior:'smooth'});
-  });
-
-});
-
-
-
+    const elm = document.querySelectorAll('.scroll');
+    const elmCount = elm.length;
+    
+    //chrome
+    elm.forEach(function(item, index){
+      item.addEventListener('mousewheel', function(event){
+        event.preventDefault();
+        let delta = 0;
+    
+        if (!event) event = window.event;
+        if (event.wheelDelta) {
+            delta = event.wheelDelta / 120;
+            if (window.opera) delta = -delta;
+        } 
+        else if (event.detail)
+            delta = -event.detail / 3;
+    
+        let moveTop = window.scrollY;
+        let elmSelector = elm[index];
+    
+        // wheel down : move to next section
+        if (delta < 0){
+          if (elmSelector !== elmCount-1){
+            try{
+              moveTop = window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
+            }catch(e){}
+          }
+        }
+        // wheel up : move to previous section
+        else{
+          if (elmSelector !== 0){
+            try{
+              moveTop = window.pageYOffset + elmSelector.previousElementSibling.getBoundingClientRect().top;
+            }catch(e){}
+          }
+        }
+    
+        const body = document.querySelector('html');
+        window.scrollTo({top:moveTop, left:0, behavior:'smooth'});
+      });
+    
+    });
 
 
 
